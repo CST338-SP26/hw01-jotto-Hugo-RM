8b54b67890c7b5c9f8ade16d685168d8d625be37
Hugo M. Ruiz-Mireles <hugo.ruizmireles@gmail.com>
2026-02-11 10:23:05 -0800

    Clean up repo and fixed compilation errors with code

--- src/main/java/Jotto.java
+++ src/main/java/Jotto.java
@@ -1,28 +1,48 @@
-import java.utils.ArrayList
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 /**
  * @author feng3302
  * @version 0.1.0
  * @Since 1/29/26
  **/
 public class Jotto {
+    private static final int WORD_SIZE = 5;
+    private static final boolean DEBUG = true;
+
+    private final ArrayList<String> playGuesses = new ArrayList<String>();
+    private final ArrayList<String> playedWords = new ArrayList<String>();
+    private final ArrayList<String> wordList;
+
+    private String currentWord;
+    private String fileName;
+
+    private int score;
+
     public Jotto(String filename) {
-        this.DEBUG = true;
-        this.WORD_SIZE = 5;
+        this.fileName = filename;
+        this.wordList = readWords();
     }
 
     public boolean pickWord() {
-
+        return false;
     }
 
     public String showWordList() {
-
+        return "";
     }
 
     public ArrayList<String> showPlayerGuesses() {
-
+        return new ArrayList<String>() {{
+            add("Hello");
+            add("World!");
+        }};
     }
 
-    void playerGuessScores(ArrayList<String>) {
+    void playerGuessScores(ArrayList<String> guesses) {
 
     }
 
@@ -30,8 +50,20 @@ public class Jotto {
         this.currentWord = currentWord;
     }
 
-    public ArrayList<String> readWords(ArrayList<String> words) {
+    public ArrayList<String> readWords() {
+        ArrayList<String> words = new ArrayList<String>();
 
+        File f = new File(this.fileName);
+
+        try (Scanner s = new Scanner(f)) {
+            while (s.hasNextLine()) {
+                words.add(s.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File Not Found Exception.");
+        }
+
+        return words;
     }
 
     public void play() {
@@ -39,39 +71,32 @@ public class Jotto {
     }
 
     int guess() {
-
+        return 0;
     }
 
     public ArrayList<String> getPlayWords() {
-
+        return new ArrayList<String>(this.playedWords);
     }
 
     public String getCurrentWord() {
-
+        return currentWord;
     }
 
-    public int getLetterCount(String) {
-
+    public int getLetterCount(String word) {
+        return 0;
     }
 
     public String showPlayedWords() {
-
+        return "PlayedWords";
     }
 
     public boolean addPlayerGuess(String playerGuess) {
-
+        return false;
     }
 
     void updateWordList() {
 
     }
 
-    private static final int WORD_SIZE;
-    private String currentWord;
-    private int score;
-    private final ArrayList<String> playGuesses;
-    private final ArrayList<String> playedWords;
-    private String fileName;
-    private final ArrayList<String> wordList;
-    private static final boolean DEBUG;
+
 }
